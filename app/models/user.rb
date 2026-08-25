55fda8c2663ec02e8e52d9251c8eab36b9457714
warzsummer-blip <302414655+warzsummer-blip@users.noreply.github.com>
2026-08-26 06:23:24 +0900

    各ページ見た目修正完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   belongs_to :post, optional: true
+  has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy 
   has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
